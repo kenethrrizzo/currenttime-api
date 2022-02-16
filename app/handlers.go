31e968a5d4927138c61518835ae3318fa19b06bf
kriera <kriera@cobiscorp.com>
2022-02-16 00:17:46 -0500

    getCurrentTime complete

--- app/handlers.go
+++ app/handlers.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -12,13 +13,21 @@ func getCurrentTime(rw http.ResponseWriter, r *http.Request) {
 	response := make(map[string]time.Time)
 	params := r.URL.Query()
 	timezone := params.Get("tz")
-	for _, t := range strings.Split(timezone, ",") {
-		loc, err := time.LoadLocation(t)
-		if err != nil {
-			rw.WriteHeader(404)
-			log.Fatalf("Error finding timezone. Err: %s", err) // corregir
+	if timezone != "" {
+		for _, t := range strings.Split(timezone, ",") {
+			loc, err := time.LoadLocation(t)
+			if err != nil {
+				rw.WriteHeader(http.StatusNotFound)
+				fmt.Fprint(rw, "Invalid timezone.")
+				json.NewEncoder(rw)
+				log.Printf("Timezone %s is invalid.", t)
+				return
+			} else {
+				response[t] = time.Now().In(loc)
+			}
 		}
-		response[t] = time.Now().In(loc)
+	} else {
+		response["current_time"] = time.Now()
 	}
 	json.NewEncoder(rw).Encode(response)
 }
